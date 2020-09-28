940587b20d7b696ec946d5c2aa7b2290554d92fb
0xTiger <tgh.lizard@googlemail.com>
2020-09-28 17:14:58 +0100

    added placeholder db query

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+#from stance_clf_ensemble import pred_lean
+import random
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:dbpass@localhost/reddit_stance_classifier'
+db = SQLAlchemy(app)
 
-app=Flask(__name__)
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    stance = db.Column(db.String(16))
+
+    def __init__(self, username, stance):
+        self.username = username
+        self.stance = stance
 
 @app.route("/")
 def index():
@@ -9,10 +22,19 @@ def index():
 @app.route("/success", methods=['POST'])
 def success():
     if request.method == 'POST':
-        username = request.form['username']
-        print(username)
-    return render_template("success.html")
+        POST_username = request.form['username']
+
+        cached_user = User.query.filter_by(username=POST_username).first()
+        if cached_user:
+            stance = cached_user.stance
+        else:
+            new_user = User(POST_username, random.choice(['L', 'R', 'C']))
+            db.session.add(new_user)
+            db.session.commit()
+
+            stance = new_user.stance
+    return render_template("index.html", text='Stance: ' + stance)
 
 if __name__ == '__main__':
-    app.debug=True
+    app.debug = True
     app.run()
