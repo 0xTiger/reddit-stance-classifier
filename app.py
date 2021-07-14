cf18a4597b00e070d77d4e68ce0a933dd9906732
0xTiger <tgh.lizard@googlemail.com>
2021-07-14 22:27:57 +0100

    transition website to regression outputs

--- app.py
+++ app.py
@@ -3,44 +3,46 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from prediction import pred_lean
 from requests.exceptions import HTTPError
-
+import numpy as np
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ['APP_SETTINGS'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True)
+    h_pos = db.Column(db.Float)
+    v_pos = db.Column(db.Float)
 
-    h_stance = db.Column(db.String(1))
-    h_confidence = db.Column(db.Float)
-
-    v_stance = db.Column(db.String(1))
-    v_confidence = db.Column(db.Float)
-
-    def __init__(self, username, h_stance, v_stance, h_confidence, v_confidence):
+    def __init__(self, username, v_pos, h_pos):
         self.username = username
-        self.h_stance = h_stance
-        self.h_confidence = h_confidence
-        self.v_stance = v_stance
-        self.v_confidence = v_confidence
+        self.h_pos = h_pos
+        self.v_pos = v_pos
 
-    def stance_name(self):
+    def stance_name(self, axis='both'):
 
-        quadrant_map = {'-L' : 'left',
-                        '--': 'unknown',
-                        '-R': 'right',
-                        'A-': 'auth',
-                        'L-': 'lib',
-                        'LL': 'libleft',
-                        'LR': 'libright',
-                        'AL': 'authleft',
-                        'AR': 'authright'}
-        return quadrant_map[(self.v_stance if (self.v_confidence > 0.6) else '-') + (self.h_stance if (self.h_confidence > 0.6) else '-')]
+        stancemap = {'libleft': (-1, -1), 
+                'libright': (-1, 1), 
+                'authleft': (1, -1), 
+                'authright': (1, 1),
+                'left': (0, -1),
+                'right': (0, 1),
+                'centrist': (0, 0),
+                'auth': (1, 0),
+                'lib': (-1, 0)}
+
+        stancemap_inv = {v:k for k,v in stancemap.items()}
+
+        if axis == 'both': stance = stancemap_inv.get((round(self.v_pos), round(self.h_pos)))
+        if axis == 'h': stance = stancemap_inv.get((0, round(self.h_pos)))
+        if axis == 'v': stance = stancemap_inv.get((round(self.v_pos), 0))
+        if axis == 'h_binary': stance = stancemap_inv.get((0, np.sign(self.h_pos)))
+        if axis == 'v_binary': stance = stancemap_inv.get((np.sign(self.v_pos), 0))
+        return stance
+
 
     def img(self):
         return self.stance_name() + '.png'
@@ -72,10 +74,10 @@ def success():
         return render_template("success.html", stance_name=current_user.stance_name(),
                                                 user=username,
                                                 img=current_user.img(),
-                                                h_fullstance= 'left' if current_user.h_stance == 'L' else 'right',
-                                                v_fullstance= 'lib' if current_user.v_stance == 'L' else 'auth',
-                                                h_confidence=f'{current_user.h_confidence:.0%}',
-                                                v_confidence=f'{current_user.v_confidence:.0%}')
+                                                h_fullstance= current_user.stance_name(axis='h_binary'),
+                                                v_fullstance= current_user.stance_name(axis='v_binary'),
+                                                h_confidence=f'{current_user.h_pos:.0%}',
+                                                v_confidence=f'{current_user.v_pos:.0%}')
     elif request.method == 'GET':
         return redirect(url_for('index'))
 
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
     DEBUG = False
